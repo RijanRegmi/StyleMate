2929c345606afa532547036113fba6f77e3f4135
Rijan Regmi <rijanregmi111@gmail.com>
2025-07-09 20:50:49 +0545

    create Login and signup page

---
+++ Frontend/src/Components/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Header from './Header.jsx';
+// import Footer from './Footer.jsx';
+// import Hero from './Hero.jsx';
+// import Newsinapp from './Newsinapp.jsx';
+import SignUp from './SignUp.jsx';
+import Login from './Login.jsx';
+// import MainApp from './MainApp.jsx';
+// import News from './News.jsx';
+// import About from './About.jsx';
+// import Profile from './Profile.jsx';
+// import AdminNews from './AdminNews.jsx';
+// import Admin from './Admin.jsx';
+
+function App() {
+  return(
+    <>
+      <Router>
+        <Routes>
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/" element={<Login/>} />
+          <Route path="/Login" element={<Login />} />
+          {/*<Route path="/Home" element={<MainApp/>}/> 
+          <Route path="/News" element={<News/>}/> 
+          <Route path="/About" element={<About/>}/> 
+          <Route path="/Profile" element={<Profile/>}/> 
+          <Route path="/AdminNews" element={<AdminNews/>}/> 
+          <Route path="/Admin" element={<Admin/>}/>  */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App
